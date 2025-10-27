fbb79b2c0a245b1370456902198f1f5b75608f31
Danielmeza2599 <69852644+Danielmeza2599@users.noreply.github.com>
2025-10-27 12:15:50 -0600

    cambio temporal para preubas

--- frontend/angular-frontend/src/environments/environment.ts
+++ frontend/angular-frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:3000/api',
+  production: true,
+  apiUrl: 'https://innovatube-angular.onrender.com/api', // PRUEBASS
   recaptcha: {
     siteKey: '6LeZbvcrAAAAAO4i2uwUxJmsDnM1O-09Xt4fr8wr'
   }
